cfe896f920d87517d31101f0c8d67684f4c36f5a
Alex Kalles <alexakis1997@gmail.com>
2016-09-08 13:37:22 +0300

    Update nfc.mk

--- product/nfc.mk
+++ product/nfc.mk
@@ -9,5 +9,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc.cfg:system/etc/nfc.cfg \
     $(LOCAL_PATH)/configs/nfcse.cfg:system/etc/nfcse.cfg \
     $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
