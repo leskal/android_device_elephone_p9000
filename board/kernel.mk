0c264e606938ede35884ed74cd95c8cb99e44b0d
leskal <alexakis1997@gmail.com>
2017-04-20 16:00:55 +0300

    Use prebuilt kernel from N Stable
    Change-Id: Ic142210eec8f2078cc6af87fef3d069b333e98b3

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,5 @@
 # Kernel informations
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+#BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_NAME := 1465391499
 BOARD_KERNEL_BASE := 0x40078000
@@ -10,7 +10,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --board $(BOARD_NAME) --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET) --second_offset 0x00e88000
 
 # Kernel properties
-TARGET_KERNEL_SOURCE := kernel/elephone/p9000
-TARGET_KERNEL_CONFIG := lineage_p9000_defconfig
-MTK_APPENDED_DTB_SUPPORT := yes
-TARGET_IS_64_BIT := true
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
+#TARGET_KERNEL_SOURCE := kernel/elephone/p9000
+#TARGET_KERNEL_CONFIG := lineage_p9000_defconfig
+#MTK_APPENDED_DTB_SUPPORT := yes
+#TARGET_IS_64_BIT := true
